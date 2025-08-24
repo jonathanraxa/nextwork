b12f99fa54004eeb754948d6d239355ed41f737d
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-24 12:17:41 -0500

    init files add with dummy data

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+// _app.js is the root component for our Next.JS application
+// it is the component that wraps all other components
+// it is the component that is used to initialize the application
+// it is the component that is used to wrap the application
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
