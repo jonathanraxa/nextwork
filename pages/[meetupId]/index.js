e70e09b4b8ff4ab948c4580afcfc1884ba864651
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-24 12:50:27 -0500

    created details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,8 +1,17 @@
-// our-domain.com/meetups/[meetupId]
-import NewMeetupForm from "@/src/components/meetups/NewMeetupForm";
+// our-domain.com/[meetupId]
+
+import { useRouter } from 'next/router';
 
 const MeetupDetails = () => {
-
+    const router = useRouter();
+    const meetupId = router.query.meetupId;
+    return (
+        <>
+            <img src={`https://picsum.photos/200/300?random=${meetupId}`} alt='Random Image' />
+            <h1>Meetup Details</h1>
+            <p>{meetupId}</p>
+        </>
+    )
 };
 
 export default MeetupDetails;
